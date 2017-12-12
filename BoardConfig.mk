daaea5208035f06365f3bebe70ef7ef0d74676ab
bryan2894 <bryan2894@gmail.com>
2017-12-12 16:05:12 -0500

    celox: add BOARD_KERNEL_IMAGE_NAME
    Change-Id: I7d707729b44554da82a6bcfe93c449b7ace1bf7c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,6 +34,7 @@ TARGET_INIT_VENDOR_LIB := libinit_celox
 # Kernel
 BOARD_KERNEL_BASE := 0x40400000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_CONFIG := celox_defconfig
